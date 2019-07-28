954583c6c5a39849073d50123ccb9c73a81e294d
Tiago Cesar <tiago.1301711@fapi-pinhais.edu.br>
2019-07-28 16:09:21 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedidoPK.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedidoPK.java
@@ -21,17 +21,17 @@ public class ItemPedidoPK implements Serializable{
 	public Pedido getPedido() {
 		return pedido;
 	}
+	
 	public void setPedido(Pedido pedido) {
 		this.pedido = pedido;
 	}
+	
 	public Produto getProduto() {
 		return produto;
 	}
+	
 	public void setProduto(Produto produto) {
 		this.produto = produto;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
 	}
 	
 	@Override
